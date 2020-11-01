5554f6d5793c068ea1df862965ae9e063081ee0f
Collin Sperry <42603366+Vatos2@users.noreply.github.com>
2020-10-31 19:16:35 -0700

    Pushing all changes

--- Functions.py
+++ Functions.py
@@ -1,35 +1,37 @@
 import Listen as l
 import os
 
+
 def helper(filename):
-    #tempfile = filename
+    tempfile = filename
     #filename.close()
     theFile = open('HelpText.txt', "r")
     print(theFile.read())
+    print("Say exit to return to the main menu")
     theFile.close()
-    print("Say exit to return to the main menu...")
 
 
 def openFile():
     print("Say File Name to open: <inputFileName>")
     fileString = l.listen() + ".py"
     if os.path.exists(fileString):
-        returnFile = open(fileString, "a")
-    else:
-        returnFile = open(fileString, "a+")
+        returnFile = open(fileString, "w")
+        return returnFile.name
+    returnFile = open(fileString, "w+")
     return returnFile.name
 
 
-def exitMenu(exitcode):
-    if exitcode == "exit":
+def exitMenu(exit):
+    if exit == "exit":
         print("Emergency exit.")
         exit(1)
 
 def runFile(filename):
     print("Running file....")
-    #runs the file the user has chosen
-    if filename != "start.py":
-        print("Output from \"", filename, "\"...")
+    # runs the file the user has chosen
+    if (len(filename) != 0):
         os.system(filename)
+
+
     else:
         print("You cannot run a file prior to creating one, call open file and write some code in it first.")
--- Write.py
+++ Write.py
@@ -6,12 +6,12 @@ def writeFile(filename):
     if filename == "main.py" or filename == "functions.py" or filename == "listen.py" or filename == "write.py":
         print("CRITICAL ERROR. ATTEMPTING TO OVERWRITE SOURCE CODE. EXITING...")
         exit(1)
-    file = open(filename, "w+")
+    file = open(filename, "a+")
     print("Current file looks like:")
     print(file.read())
     # Write Loop
     while 1:
-        print("What would you like to write? Say <exit write> to exit back to the main menu")
+        print("What would you like to write?")
         print("Waiting for input...")
         text = l.listen()
 
@@ -20,17 +20,23 @@ def writeFile(filename):
             file.close()
             break
 
-        if text == "comment" or text == "common" or text == "comments":
+        if text == "comment" or text == "common" or text =="comments":
             print("Commenting...")
             comment(filename)
-
-        if text == "assign" or text == "a sign":
-            print("Entering assignment...")
+        if text == "variable":
+            print("Entering variable...")
             assignString = variableDeclaration(filename)
             file.write(assignString)
             file.write("\n")
 
-        print("Finished parsing <", text, ">")
+        if text == "string":
+            print("Enter the contents of your string")
+            text = l.listen()
+            file.write( "print(" + "\"" + text + "\"" + ")" )
+            print("String has been written too...")
+            file.write("\n")
+
+        print("Finished parsing text: " + text)
 
 
 def comment(filename):
@@ -54,14 +60,77 @@ def comment(filename):
 def variableDeclaration(filename):
     currentline = len(filename.splitlines())
     print("You are writing to line#:", currentline)
-    print("Please say your variable name:")
+    print("Please say your variable name: ")
     tempvar = l.listen()
-    print("Variable Name:", tempvar)
-    print("Say the value:")
+    print("Variable Name: ", tempvar)
+    print("Value: ")
     tempVal = l.listen()
     print("Value: ", tempVal)
     return tempvar + " = " + tempVal
 
-def writeLine(filename):
-    currentline = len(filename.splitlines())
-    print("Say the line you want to write to line #", currentline)
+
+
+
+
+def editFile(filename):
+    print("Editing file...." + '\n')
+    print("Say the line number of the file you want to edit: ")
+    lineNumber = l.listen()
+    print("INPUT HEARD: " + lineNumber)
+    lineNumber1 = int(change_to_number(lineNumber))
+    my_file1 = open(filename, 'r')
+    string_list = my_file1.readlines()
+    my_file1.close()
+    my_file = open(filename, 'w')
+    print(len(string_list))
+    print("The line number is: " + str(lineNumber1))
+    print(type(lineNumber1))
+    print("File Line being edited is: " + string_list[lineNumber1-1])
+
+    print("Resay your edited line: ")
+    newLine = l.listen()
+    string_list[lineNumber1-1] = newLine
+    print("Line has been edited...")
+
+
+    for i in range (len(string_list)):
+        myString = string_list[i]
+        print(myString)
+        my_file.write(myString)
+        my_file.write("\n")
+    my_file.close()
+
+def change_to_number(stringy):
+
+    if(stringy == "won" or stringy == "one"):
+        return 1
+
+    elif(stringy == "two" or stringy == "too" or stringy == "to"):
+        return 2
+
+    elif(stringy == "three" or stringy == "free" ):
+        return 3
+
+    elif(stringy == "for" or stringy == "four" or stringy ==  "fore"):
+        return 4
+
+    elif(stringy == "five"):
+        return 5
+
+    elif(stringy == "six" or stringy == "sex"):
+        return 6
+
+    elif(stringy == "seven"):
+        return 7
+
+    elif(stringy == "ate" or stringy == "eight"):
+        return 8
+
+    elif(stringy == "nine" or stringy == "nein"):
+        return 9
+
+    else:
+        return stringy
+
+
+
