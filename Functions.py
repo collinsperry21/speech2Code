313071b88bc39665f56320e77e412399018fccc2
Ivanov Munoz <munoz.aar@gmail.com>
2020-11-13 23:09:23 -0600

    edited the HelpText.txt and helper function

--- Functions.py
+++ Functions.py
@@ -1,59 +1,41 @@
 import Listen as l
-
-
-def variableDeclaration(filename):
-    currentline = len(filename.splitlines()) + 1
-    print("You are writing to line#:", currentline)
-    print("Please say your variable name: <varName>")
-    tempvar = l.listen()
-    print("Variable Name: <inputVariable>", tempvar)
-    print("Value: <inputValue>")
-    tempVal = l.listen()
-    print("Value: ", tempVal)
-    return tempvar + " = " + tempVal
-
-
-def comment(filename):
-    while 1:
-        currentline = len(filename.splitlines())+1
-        print("You are commenting to line#:", currentline)
-        print("")
-        commentstring = l.listen()
-        filename.write("# "+commentstring+"\n")
-        print("Would you like to make another line of comments? (Say <yes> / <no>)")
-        choice = l.listen()
-        if choice == "yes":
-             comment(filename)
-        elif choice == "no":
-             break
-        else:
-            print("Unable to process request. Input heard: ", choice)
-            continue
+import os
 
 
 def helper(filename):
     tempfile = filename
-    filename.close()
+    #filename.close()
     theFile = open('HelpText.txt', "r")
     print(theFile.read())
+    os.startfile('HelpText.txt')
     print("Say exit to return to the main menu")
-    tempfile.open()
+    BacktoMenu = l.listen()
+    if BacktoMenu == "exit help" or "exit hell":
+        print("closing help manual and returning to main")
+        os.system('TASKKILL /F /IM notepad.exe')
+        theFile.close()
+
 
 
 def openFile():
     print("Say File Name to open: <inputFileName>")
     fileString = l.listen() + ".py"
     fileString.replace(" ", "")
-    returnFile = open(fileString,"w+")
+    returnFile = open(fileString, "w+")
     return returnFile.name
 
 
-def startCheck(filename):
-    if filename == "start.py":
-        return 1
-    return 0
-
 def exitMenu(exit):
     if exit == "exit":
-        return 1
-    return 0
+        print("Emergency exit.")
+        exit(1)
+
+def runFile(filename):
+    print("Running file....")
+    # runs the file the user has chosen
+    if (len(filename) != 0):
+        os.system(filename)
+
+
+    else:
+        print("You cannot run a file prior to creating one, call open file and write some code in it first.")
